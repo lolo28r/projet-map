ba6171227e25ed6021495f7382e5779d1339d8e7
lolo28r <laurienroche@gmail.com>
2026-01-20 16:38:17 +0100

    update nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,52 @@
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-import AppRoutes from './routes/appRoutes.jsx';
-import './App.css';
-import { UserProvider } from './context/userContext.jsx';
+import { BrowserRouter as Router, Link, useNavigate } from "react-router-dom";
+import AppRoutes from "./routes/appRoutes.jsx";
+import "./App.css";
+import { UserProvider, UserContext } from "./context/userContext.jsx";
+import { useContext } from "react";
+
+function Navbar() {
+  const { token, setToken, setUserId } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    setToken(null);
+    setUserId(null);
+    navigate("/login");
+  };
+
+  return (
+    <nav className="nav">
+      <div className="nav-left">
+        <Link to="/">Accueil</Link>
+      </div>
+
+      <div className="nav-right">
+        {!token ? (
+          <>
+            <Link to="/register">Inscription</Link>
+            <Link to="/login">Connexion</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/profile">Mon compte</Link>
+            <button className="logout-btn" onClick={handleLogout}>
+              Déconnexion
+            </button>
+          </>
+        )}
+      </div>
+    </nav>
+
+  );
+}
 
 function App() {
   return (
     <UserProvider>
       <Router>
-        <nav className='nav'>
-          <Link to="/">Accueil</Link>
-          <Link to="/register">Inscription</Link>
-          <Link to="/login">Connexion</Link>
-          <Link to="/profile">Profil</Link>
-        </nav>
+        <Navbar />
         <AppRoutes />
       </Router>
     </UserProvider>
